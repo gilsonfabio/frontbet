288ce5bbd021ff0d8d525027ea5a4b117d9ccb4e
Gilson Fábio <gilsonfabio@gmail.com>
2023-09-19 15:51:05 -0300

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+
+import ThemeProvider from './components/ThemeProvider'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer'
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="">
+        <ThemeProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
